9c63d8efc51188d1128b2433c257f31d65cb8429
Gabriel Aumala <gaumala@protonmail.com>
2019-07-10 06:08:26 -0500

    corregir unit tests en backend

--- backend/src/pdf/pdfutils.js
+++ backend/src/pdf/pdfutils.js
@@ -1,9 +1,65 @@
 const fs = require('fs');
-const {
-  fromVentaRow,
-  crearMatrizValoresTotales,
-  generarDetalleOpcionesDePago
-} = require('./facturaPDFData.js');
+
+const { calcularValoresTotales } = require('facturacion_common/src/Math.js');
+const Money = require('facturacion_common/src/Money.js');
+
+const generarDetalleOpcionesDePago = pagos => {
+  const tarjetaOptions = [
+    'tarjeta_legacy',
+    'tarjeta_debito',
+    'tarjeta_credito'
+  ];
+
+  const efectivoPayment = pagos.find(i => i.formaPago === 'efectivo') || {
+    valor: null
+  };
+  const dineroElecPayment = pagos.find(
+    i => i.formaPago === 'dinero_electronico_ec'
+  ) || { valor: null };
+  const tarjetaPayment = pagos
+    .filter(i => tarjetaOptions.includes(i.formaPago))
+    .reduce(
+      (acc, i) => ({
+        valor: acc.valor != null ? acc.valor + i.valor : i.valor
+      }),
+      { valor: null }
+    );
+
+  const transferPayment = pagos.find(i => i.formaPago === 'transferencia') || {
+    valor: null
+  };
+  const otrosPayment = pagos.find(i => i.formaPago === 'otros') || {
+    valor: null
+  };
+
+  return [
+    ['EFECTIVO', efectivoPayment.valor],
+    ['DINERO ELECTRÓNICO', dineroElecPayment.valor],
+    ['TARJETA DE CRÉDITO/DÉBITO', tarjetaPayment.valor],
+    ['TRANSFERENCIA', transferPayment.valor],
+    ['OTRO', otrosPayment.valor]
+  ];
+};
+
+const crearMatrizValoresTotales = ({ subtotal, flete, iva, descuento }) => {
+  const { rebaja, impuestos, total } = calcularValoresTotales(
+    subtotal,
+    flete,
+    iva,
+    descuento
+  );
+
+  const matrix = [];
+  matrix.push(['Sub-Total', '$', Money.print(subtotal)]);
+  matrix.push(['Descuento', `${descuento}%`, Money.print(rebaja)]);
+  if (iva === 0) matrix.push(['IVA', '0%', '0.00']);
+  else matrix.push(['IVA', '%', '']);
+  matrix.push(['Flete', '$', Money.print(flete)]);
+  if (iva > 0) matrix.push(['IVA', `${iva}%`, Money.print(impuestos)]);
+  else matrix.push(['IVA', '%', '']);
+  matrix.push(['Total US', '$', Money.print(total)]);
+  return matrix;
+};
 
 const billetesPalabras = billetes => {
   if (billetes === 0) return 'CERO';
@@ -101,6 +157,5 @@ module.exports = {
   valorPalabras,
   createTemporaryDir,
   crearMatrizValoresTotales,
-  generarDetalleOpcionesDePago,
-  ventaRowToPDFData: fromVentaRow
+  generarDetalleOpcionesDePago
 };
