f50b896e11ba5a5badb59e45492af68aca0604b3
Gabriel Aumala <gabrielfima@live.com>
2019-07-16 10:08:23 -0500

    Frontend fixes
    Permitir strings vacios al validar busquedas de productos.
    Siempre llamar a editarFn al cerrar dialog de comprobante.

--- frontend/src/Factura/FacturaEditorView.js
+++ frontend/src/Factura/FacturaEditorView.js
@@ -111,7 +111,7 @@ export default class FacturaEditorView extends Component {
   editarFactura = ventaId => {
     const { ventaKey, isExamen } = this.props;
     if (ventaId === ventaKey)
-      return updateState(this, { type: Actions.cerrarComprobanteDialog });
+      updateState(this, { type: Actions.cerrarComprobanteDialog });
 
     const editarFn = isExamen
       ? this.props.editarFacturaExamen
